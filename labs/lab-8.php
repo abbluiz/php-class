d34cc6a38f2f6609234b46ea4062f7f22f272934
abbluiz <unt@labb.email>
2019-10-16 20:04:30 -0400

    Add Lab 8

---
+++ labs/lab-8.php
@@ -0,0 +1,27 @@
+<?php
+
+    // 1
+    class MyClass
+    {
+        public $attribute1;
+    }
+
+    // 2
+    $c = new MyClass;
+
+    // 3
+    // Answer: extends
+
+    // 4
+    // Answer: attributes
+
+    // 5
+    // Answer: polymorphism
+
+    // 6
+    // Answer: functions
+
+    // 7
+    // Answer: contructor
+
+?>
